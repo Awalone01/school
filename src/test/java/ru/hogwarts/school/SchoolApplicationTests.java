b2fea570f39f16103130aaa5dad59641a6abc3a2
Awalone01 <sashaneiv01@yandex.ru>
2022-05-13 18:51:20 +0300

    Commit 1 HW6

--- src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
+++ src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
@@ -1,10 +1,23 @@
 package ru.hogwarts.school;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import ru.hogwarts.school.controller.StudentController;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SchoolApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private StudentController studentController;
+
+	@Autowired
+	private TestRestTemplate testRestTemplate;
 
 	@Test
 	void contextLoads() {
