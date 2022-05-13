775322f9e3fd1b88b57a68c0a16677067a08546d
Awalone01 <sashaneiv01@yandex.ru>
2022-05-13 22:15:55 +0300

    Commit 5 HW6

--- src/test/java/ru/hogwarts/school/FacultyApplicationTests.java
+++ src/test/java/ru/hogwarts/school/FacultyApplicationTests.java
@@ -1,14 +1,28 @@
 package ru.hogwarts.school;
 
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ru.hogwarts.school.controller.FacultyController;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.repository.FacultyRepository;
 import ru.hogwarts.school.service.FacultyService;
+
+import java.util.Optional;
+
+import static java.awt.Color.red;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 public class FacultyApplicationTests {
@@ -25,5 +39,31 @@ public class FacultyApplicationTests {
     @InjectMocks
     private FacultyController facultyController;
 
+    @Test
+    public void saveStudentTest() throws Exception {
+        Long id = 1L;
+        String name = "Gryffindor";
+        String color = "red";
 
+        JSONObject facultyObject = new JSONObject();
+        facultyObject.put("name", name);
+        facultyObject.put("color", color);
+
+        Faculty faculty = new Faculty(id, name, color);
+
+        when(facultyRepository.save(any(Faculty.class))).thenReturn(faculty);
+        when(facultyRepository.findById(id)).thenReturn(Optional.of(faculty));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/faculty")
+                        .content(facultyObject.toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(id))
+                .andExpect(jsonPath("$.name").value(name))
+                .andExpect(jsonPath("$.color").value(color));
+    }
+
+    //java.lang.IllegalStateException: Failed to load ApplicationContext
 }
