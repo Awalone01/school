1bb37fd7a32fdef3628c09021730d931583b6867
Awalone01 <sashaneiv01@yandex.ru>
2022-05-13 20:29:06 +0300

    Commit 2 HW6

--- src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
+++ src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
@@ -1,11 +1,14 @@
 package ru.hogwarts.school;
 
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import ru.hogwarts.school.controller.StudentController;
+import ru.hogwarts.school.model.Student;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SchoolApplicationTests {
@@ -20,7 +23,44 @@ class SchoolApplicationTests {
 	private TestRestTemplate testRestTemplate;
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception{
+		Assertions.assertThat(studentController).isNotNull();
+	}
+
+	@Test
+	public void testPostStudent() throws Exception {
+		Student student = new Student();
+		student.setId(1L);
+		student.setName("Bob");
+		student.setAge(13);
+
+		Assertions.
+				assertThat(this.testRestTemplate.postForObject("http://localhost:" + port + "/id", student, String.class))
+				.isNotNull();
+	}
+
+	@Test
+	public void testGetStudent() throws Exception {
+		Student student = new Student();
+		student.setId(1L);
+		student.setName("Bob");
+		student.setAge(13);
+
+		Assertions.
+				assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/id" +  student, String.class))
+				.isNotNull();
+	}
+
+	@Test
+	public void testDeleteStudent() throws Exception {
+		Student student = new Student();
+		student.setId(1L);
+//		student.setName("Bob");
+//		student.setAge(13);
+
+		Assertions.
+				assertThat(this.testRestTemplate.delete("http://localhost:" + port + "/id", student, String.class))
+				.isNotNull();
 	}
 
 }
