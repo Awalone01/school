6a0d8ca32fe2e3250e2db490519581f40507f8f4
Awalone01 <sashaneiv01@yandex.ru>
2022-06-06 20:32:11 +0300

    Commit 3 4.0HW3

--- src/main/resources/liquibase/scripts/course-four-lesson-three.sql
+++ src/main/resources/liquibase/scripts/course-four-lesson-three.sql
@@ -1,6 +1,7 @@
-– liquibase formatted sql
+--liquibase formatted sql
 
-- changeset avolkov:1
+-- changeset avolkov:1
 create index student_name_index on student (name);
 
-
+-- changeset avolkov:2
+create index faculty_name_and_color_index on faculty (name, color);
