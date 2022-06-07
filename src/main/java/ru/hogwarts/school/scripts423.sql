a42f2bd6855b4d8bfc846b03c6d0ab21332364ab
Awalone01 <sashaneiv01@yandex.ru>
2022-06-07 22:20:10 +0300

    Commit 1 4.0HW4

--- src/main/java/ru/hogwarts/school/scripts423.sql
+++ src/main/java/ru/hogwarts/school/scripts423.sql
@@ -1,5 +1,14 @@
-select json_build_array(student.name, age, faculty.name) from student, faculty;
+SELECT student.name, student.age, faculty.name
+FROM student
+         INNER JOIN faculty ON student.faculty_id = faculty.id;
 
-select json_build_array(avatar.student_id, cast(1 as bit)) from avatar
+select  student.name, avatar.file_path
+from student
+         right join avatar on avatar.student_id = student.id;
+
+-- Показать, что список не из 7 студентов
+select  student.name, avatar.file_path
+from student
+         left join avatar on avatar.student_id = student.id;
 
 
